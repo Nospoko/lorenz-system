db6541488be00b280fc1102fa97d57f7e61038ae
WojciechMat <wmatsad@gmail.com>
2024-02-20 09:52:10 +0100

    DRY

--- lorenz.py
+++ lorenz.py
@@ -5,6 +5,44 @@ import matplotlib.pyplot as plt
 from solvers import RungeKutta4, ExplicitEuler
 
 
+def lorenz_solutions_review(result: np.ndarray, nodes: np.ndarray):
+    result_t = result.T
+    fig, axes = plt.subplots(3, figsize=(14, 7))
+    for it, ax in enumerate(axes):
+        ax.plot(nodes, result_t[it], label=f"y_{it + 1}")
+        ax.legend()
+    axes[0].set_title("Y(x)")
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(3, figsize=(14, 7))
+    axes[0].plot(result_t[0], result_t[1])
+    axes[0].set_ylabel("y_2")
+    axes[0].set_xlabel("y_1")
+
+    axes[1].plot(result_t[0], result_t[2])
+    axes[1].set_ylabel("y_3")
+    axes[1].set_xlabel("y_1")
+
+    axes[2].plot(result_t[1], result_t[2])
+    axes[2].set_ylabel("y_2")
+    axes[2].set_xlabel("y_3")
+
+    fig.tight_layout(pad=1.2)
+    st.pyplot(fig)
+
+    # Plot
+    fig = plt.figure(figsize=(14, 7))
+    ax = fig.add_subplot(projection="3d")
+
+    ax.plot(*result.T, lw=0.5)
+    ax.set_xlabel("X Axis")
+    ax.set_ylabel("Y Axis")
+    ax.set_zlabel("Z Axis")
+    ax.set_title("Lorenz Attractor")
+
+    st.pyplot(fig)
+
+
 def main():
     parameter_cols = st.columns(3)
     # Define the default parameters values
@@ -34,46 +72,13 @@ def main():
     rk4 = RungeKutta4(lorenz, x0=x0, y0=y0, h=h, num=num)
     result = rk4.integrate()
 
-    fig, axes = plt.subplots(3, figsize=(14, 7))
-    for it, ax in enumerate(axes):
-        ax.plot(rk4.nodes, result.T[it], label=f"y_{it + 1}")
-        ax.legend()
-    axes[0].set_title("Y(x)")
-    st.pyplot(fig)
-
-    # Plot
-    fig = plt.figure(figsize=(14, 7))
-    ax = fig.add_subplot(projection="3d")
-
-    ax.plot(*result.T, lw=0.5)
-    ax.set_xlabel("X Axis")
-    ax.set_ylabel("Y Axis")
-    ax.set_zlabel("Z Axis")
-    ax.set_title("Lorenz Attractor")
-
-    st.pyplot(fig)
+    lorenz_solutions_review(result=result, nodes=rk4.nodes)
 
     st.write("Explicit Euler")
     eul = ExplicitEuler(lorenz, x0=x0, y0=y0, h=h, num=num)
     result = eul.integrate()
 
-    fig, axes = plt.subplots(3, figsize=(14, 7))
-    for it, ax in enumerate(axes):
-        ax.plot(rk4.nodes, result.T[it], label=f"y_{it + 1}")
-        ax.legend()
-    axes[0].set_title("Y(x)")
-    st.pyplot(fig)
-
-    # Plot
-    fig = plt.figure(figsize=(14, 7))
-    ax = fig.add_subplot(projection="3d")
-
-    ax.plot(*result.T, lw=0.5)
-    ax.set_xlabel("X Axis")
-    ax.set_ylabel("Y Axis")
-    ax.set_zlabel("Z Axis")
-    ax.set_title("Lorenz Attractor")
-    st.pyplot(fig)
+    lorenz_solutions_review(result=result, nodes=eul.nodes)
 
 
 if __name__ == "__main__":
